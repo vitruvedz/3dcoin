95d48c95283a773bc0b3f18ea7042647a516297b
BlockchainTechLLC <admin@districts.io>
2018-07-14 02:29:00 +0400

    stop force disconnect for stalled nodes on sync

--- src/masternode-sync.cpp
+++ src/masternode-sync.cpp
@@ -369,8 +369,7 @@ void CMasternodeSync::ProcessTick()
                 continue;
             }
 
-            // Make sure this peer is presumably at the same height
-            if(!CheckNodeHeight(pnode, true)) continue;
+            
 
             // SPORK : ALWAYS ASK FOR SPORKS AS WE SYNC (we skip this mode now)
 
