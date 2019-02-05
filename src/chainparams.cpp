9c7a6f0d0b92d3ed3bc8a699859d7df12474cf25
0xZain <admin@districts.io>
2019-02-05 09:01:20 +0400

    update checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -177,9 +177,10 @@ public:
             (  50, uint256S("0x000000d10472d9b2de295fef7bc7d6ce8c90fed450ae22e1e9a5731d5cd9ad05"))
             (  40805, uint256S("0x000000000a7c49002925d143b45ef89c1f47e6af9765c6ee0331af1530e70ebf"))
             (  170690, uint256S("0x0000000000115dd61216bb43ab76eb4921ffc33e47fd57e4c108c49371e5bd86"))
-            (  409223, uint256S("0x000000000000033ac12f288d05943b22aa223de94b90c1c41594df08781d7528")),
-            1548422870, // * UNIX timestamp of last checkpoint block
-            273027,    // * total number of transactions between genesis and last checkpoint
+            (  409223, uint256S("0x000000000000033ac12f288d05943b22aa223de94b90c1c41594df08781d7528"))
+            (  423781, uint256S("0x0000000000007a5f6302854a8f9a559ce640e82d503064ffe1316b076195ae6e")),
+            1549341541, // * UNIX timestamp of last checkpoint block
+            279514,    // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
             0        // * estimated number of transactions per day after checkpoint
         };
