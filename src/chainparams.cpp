73ba0b671a227f82fdfe6e0d6b482b2599ce2817
0xZain <admin@districts.io>
2019-03-08 10:34:54 +0400

    reset testnet checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -287,10 +287,9 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (   0, uint256S("0x000007c41652b32383518b0a5b6722c5a693c730c4be7f1abcb0f70e286a3edd"))
-            (   19437, uint256S("0x000000008e3bcaf43bcb752c2aa2b96453ed715087f40d9c4c481837d36dc38f")),
-            1551338294, // * UNIX timestamp of last checkpoint block
-            7217,     // * total number of transactions between genesis and last checkpoint
+            (   0, uint256S("0x000007c41652b32383518b0a5b6722c5a693c730c4be7f1abcb0f70e286a3edd")),
+            0, // * UNIX timestamp of last checkpoint block
+            0,     // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
             0         // * estimated number of transactions per day after checkpoint
         };
